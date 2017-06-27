8dc92f3893a14a6c0c39c6b62471242ee8a4b6b4
Thomas Smallridge <thomas@studentedge.com.au>
2017-06-27 16:16:39 +0800

    Added halloffame and jsonfile library

--- bot.js
+++ bot.js
@@ -2,17 +2,47 @@ const token = 'MzI1MTAzNjM5OTYwMjIzNzQ2.DCuLWw.2VMAkzg-16d_uB-z1mEdIE8LcBU';
 //https://www.npmjs.com/package/website-scraper
 const Discord = require("discord.js");
 const moment = require('moment')
+const jsonfile = require('jsonfile')
 const client = new Discord.Client();
+
 const prefix = '.'
+var halloffame = {}
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
+    var filepath = __dirname + '\\data\\halloffame.json'
+    jsonfile.readFile(filepath, function(err, obj) {
+        if (!err) {
+            halloffame = obj;
+            console.log("Loaded Hall of Fame")
+        } else console.log(err);
+    })
 });
 
 client.on('message', msg => {
     var lowercaseContent = msg.content.toLowerCase()
+    if (msg.author.id == '325103639960223746') return; //dont let the bot read its own messages
+
+    if (lowercaseContent.startsWith(prefix + "halloffame")) {
+        if (lowercaseContent.indexOf("add") > -1) {
+            console.log("adding new guy to hall of fame")
+        } else {
+            if (halloffame != null) {
+                var rip = msg.guild.emojis.find(findRip);
+                msg.channel.send("**===[Grimmy HC Hall of Fame]===**\n"
+                + "Conquerers of Ultimate & Masters of leveling"
+                )
+                halloffame.members.forEach(function(item, index, arr) {
+                    var part1 = "**"+item.name+"** (" + item.class  + ") "
+                    if (item.deceased) {part1 = part1 + rip}
+                    msg.channel.send(part1 + "\n" + item.image)
+                })
+            }
+        }
+    }
+
     if (lowercaseContent === prefix + 'help') {
-        msg.channel.send("Hello memers these are my memes: \n``` .september yet? \n .thursday yet? \n .misadventures (im stupid)```")
+        msg.channel.send("Hello memers these are my memes: \n``` .september yet? \n .thursday yet? \n .halloffame ```")
     } else if (lowercaseContent === prefix + 'september yet?') {
         var month = moment().utcOffset('+0800').month();
         if (month === 8) {
@@ -67,5 +97,8 @@ function findGrim(emoji){
     return emoji.name == "grim";
 }
 
+function findRip(emoji){
+    return emoji.name == "rip";
+}
 
 client.login(token);
