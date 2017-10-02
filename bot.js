6af8b0b90df75ff8bccfccf1ec401807cda57816
Thomas Smallridge <thomas@studentedge.com.au>
2017-10-02 14:26:51 +0800

    added uptime command

--- bot.js
+++ bot.js
@@ -19,8 +19,10 @@ client.on('message', msg => {
     if (msg.author.id == '325103639960223746') return; //dont let the bot read its own messages
     var lowercaseContent = msg.content.toLowerCase();
 
-    if (lowercaseContent === '.help') {
+    if (lowercaseContent === ".help") {
         msg.channel.send("Hello memers these are my memes: \n```css \n .stash help \n .db [search string] \n .latest \n .halloffame \n .floorball yet? \n .melee ```")
+    } else if (lowercaseContent === ".uptime") {
+        msg.channel.send("I've been livin' for " + client.uptime);
     }
     db.MessageHandler(lowercaseContent, msg)
     if (lowercaseContent.startsWith(".halloffame")) {halloffame.MessageHandler(lowercaseContent, msg)}
