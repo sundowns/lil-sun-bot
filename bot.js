d9a503e33701c4c379c596039f9aadc05252395f
Sundowns <tom@smallridge.com.au>
2020-02-28 18:00:31 +0800

    disable feed

--- bot.js
+++ bot.js
@@ -1,46 +1,59 @@
-const fs = require('fs');
+const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const moment = require("moment");
-const misc = require('./misc');
-const halloffame = require('./halloffame');
-const stash = require('./stash');
-const db = require('./db');
-const feed = require('./feed');
-const streams = require('./streams');
+const misc = require("./misc");
+const halloffame = require("./halloffame");
+const stash = require("./stash");
+const db = require("./db");
+// const feed = require('./feed');
+const streams = require("./streams");
 const tokens = require("./tokens");
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setPresence({status: 'online', game: {name: 'keeping it real (grim)'}});
-    halloffame.Init();
-    stash.Init();
-    feed.Init(client);
-    streams.Init(client);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    status: "online",
+    game: { name: "keeping it real (grim)" }
+  });
+  halloffame.Init();
+  stash.Init();
+  //   feed.Init(client);
+  streams.Init(client);
 });
 
-client.on('message', msg => {
-    if (msg.author.id == '325103639960223746') return; //dont let the bot read its own messages
-    var lowercaseContent = msg.content.toLowerCase();
+client.on("message", msg => {
+  if (msg.author.id == "325103639960223746") return; //dont let the bot read its own messages
+  var lowercaseContent = msg.content.toLowerCase();
 
-    if (lowercaseContent === ".help") {
-        msg.channel.send("Hello memers these are my memes: \n```css \n .stash help \n .db [search string] \n .latest \n .halloffame \n .floorball yet? \n .streams \n .save [grimtools link] ```")
-    } else if (lowercaseContent === ".uptime") {
-        msg.channel.send("I've been livin' for " + Math.round(moment.duration(client.uptime).asMinutes()) + " minute(s).");
-    }
-    db.MessageHandler(lowercaseContent, msg)
-    if (lowercaseContent.startsWith(".halloffame")) {halloffame.MessageHandler(lowercaseContent, msg)}
-    if (lowercaseContent.startsWith(".stash")) {stash.MessageHandler(msg.content, msg)}
-    misc.MessageHandler(lowercaseContent, msg);
-    feed.MessageHandler(lowercaseContent, msg);
-    streams.MessageHandler(lowercaseContent, msg);
+  if (lowercaseContent === ".help") {
+    msg.channel.send(
+      "Hello memers these are my memes: \n```css \n .stash help \n .db [search string] \n .latest \n .halloffame \n .floorball yet? \n .streams \n .save [grimtools link] ```"
+    );
+  } else if (lowercaseContent === ".uptime") {
+    msg.channel.send(
+      "I've been livin' for " +
+        Math.round(moment.duration(client.uptime).asMinutes()) +
+        " minute(s)."
+    );
+  }
+  db.MessageHandler(lowercaseContent, msg);
+  if (lowercaseContent.startsWith(".halloffame")) {
+    halloffame.MessageHandler(lowercaseContent, msg);
+  }
+  if (lowercaseContent.startsWith(".stash")) {
+    stash.MessageHandler(msg.content, msg);
+  }
+  misc.MessageHandler(lowercaseContent, msg);
+  //   feed.MessageHandler(lowercaseContent, msg);
+  streams.MessageHandler(lowercaseContent, msg);
 });
 
 if (tokens) {
-    if (tokens.prod) {
-        client.login(tokens.prod);
-    } else {
-        console.log("Failed to locate prod token. Goodbye xoxo");
-        process.exit();
-    }
+  if (tokens.prod) {
+    client.login(tokens.prod);
+  } else {
+    console.log("Failed to locate prod token. Goodbye xoxo");
+    process.exit();
+  }
 }
--- misc.js
+++ misc.js
@@ -1,95 +1,89 @@
-const moment = require('moment')
-const prefix = '.'
+const moment = require("moment");
+const prefix = ".";
 
 /* Helper functions */
 function findMax(emoji) {
-    return emoji.name == "figureitout";
+  return emoji.name == "figureitout";
 }
 
 function findMemba(emoji) {
-    return emoji.name == "memba";
+  return emoji.name == "memba";
 }
 
-function findGrim(emoji){
-    return emoji.name == "grim";
+function findGrim(emoji) {
+  return emoji.name == "grim";
 }
 
-function findRip(emoji){
-    return emoji.name == "rip";
+function findRip(emoji) {
+  return emoji.name == "rip";
 }
 
-function findGudShit(emoji){
-   return emoji.name == "gud_shit";
+function findGudShit(emoji) {
+  return emoji.name == "gud_shit";
 }
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = {
-    MessageHandler : function(lowercaseContent, msg) {
-        if (lowercaseContent === prefix + 'floorball yet?') {
-            var day = moment().utcOffset('+0800').isoWeekday()
-            if (day === 4 || day === 3) {
-               msg.reply("you betcha homie :confetti_ball: :hockey: :goal: :man_dancing: " + msg.guild.emojis.find(findGudShit));
-            } else {
-                msg.reply(":sob: :sob: :sob: :sob:");
-            }
-         } else if (lowercaseContent.startsWith(prefix + 'f')) {
-           var result = msg.content.match(/\.f (.+)/);
-           if (result) {
-                msg.channel.send("Press 🇫 to pay respects to " + result[1]).then(message => message.react("🇫"));
-           } else if (lowercaseContent === ".f") {
-                msg.channel.send("Press 🇫 to pay respects").then(message => message.react("🇫"));
-           }
-         }
-         else { //free form text detection
-            if (lowercaseContent.indexOf("memba") > -1 || lowercaseContent.indexOf("remember") > -1) {
-                var memba = msg.guild.emojis.find(findMemba);
-                if (memba) {
-                    msg.react('🇮').then(() => {
-                       setTimeout(() => {
-                          msg.react(memba);
-                       }, 200)
-                    });
-                }
-            }
-            if (lowercaseContent.indexOf("max") > -1) {
-                var emoji = msg.guild.emojis.find(findMax);
-                if (emoji) {
-                    msg.react(emoji);
-                }
-            }
-            if (lowercaseContent.indexOf("grim") > -1) {
-                var emoji = msg.guild.emojis.find(findGrim);
-                if (emoji) {
-                    msg.react(emoji);
-                }
-            }
-            if (lowercaseContent.indexOf("love it") > -1 || lowercaseContent.indexOf("amazing") > -1 || lowercaseContent.indexOf("great") > -1) {
-                var emoji = msg.guild.emojis.find(findGudShit);
-                if (emoji) {
-                    msg.react(emoji);
-                }
-            }
-            if (lowercaseContent.indexOf("mornin") > -1 && lowercaseContent.indexOf("jeff") > -1) {
-                msg.reply("mornin'");
-            }
-            if (lowercaseContent.indexOf("pm ") > -1 || lowercaseContent.indexOf(" pm") > -1
-                || lowercaseContent.indexOf(" pm") > -1
-                || lowercaseContent.indexOf("project m") > -1
-                || lowercaseContent.indexOf("projectm") > -1
-                || lowercaseContent === "pm") {
-
-                //var index = getRandomInt(0, responses.length - 1);
-                //var response = responses[index];
-                //msg.reply(response);
-                //msg.delete();
-                var emoji = msg.guild.emojis.find(findRip);
-                if (emoji) {
-                    msg.react(emoji);
-                }
-            }
-         }
+  MessageHandler: function(lowercaseContent, msg) {
+    if (lowercaseContent === prefix + "floorball yet?") {
+      var day = moment()
+        .utcOffset("+0800")
+        .isoWeekday();
+      if (day === 4) {
+        msg.reply(
+          "you betcha homie :confetti_ball: :hockey: :goal: :man_dancing: " +
+            msg.guild.emojis.find(findGudShit)
+        );
+      } else {
+        msg.reply(":sob: :sob: :sob: :sob:");
+      }
+    } else if (lowercaseContent.startsWith(prefix + "f")) {
+      var result = msg.content.match(/\.f (.+)/);
+      if (result) {
+        msg.channel
+          .send("Press 🇫 to pay respects to " + result[1])
+          .then(message => message.react("🇫"));
+      } else if (lowercaseContent === ".f") {
+        msg.channel
+          .send("Press 🇫 to pay respects")
+          .then(message => message.react("🇫"));
+      }
+    } else {
+      //free form text detection
+      if (
+        lowercaseContent.indexOf("memba") > -1 ||
+        lowercaseContent.indexOf("remember") > -1
+      ) {
+        var memba = msg.guild.emojis.find(findMemba);
+        if (memba) {
+          msg.react("🇮").then(() => {
+            setTimeout(() => {
+              msg.react(memba);
+            }, 200);
+          });
+        }
+      }
+      if (lowercaseContent.indexOf("max") > -1) {
+        var emoji = msg.guild.emojis.find(findMax);
+        if (emoji) {
+          msg.react(emoji);
+        }
+      }
+      if (lowercaseContent.indexOf("grim") > -1) {
+        var emoji = msg.guild.emojis.find(findGrim);
+        if (emoji) {
+          msg.react(emoji);
+        }
+      }
+      if (
+        lowercaseContent.indexOf("mornin") > -1 &&
+        lowercaseContent.indexOf("jeff") > -1
+      ) {
+        msg.reply("mornin'");
+      }
     }
-}
+  }
+};
